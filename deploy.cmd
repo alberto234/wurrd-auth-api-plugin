6abd28e7053ccf1993be5ec9348f5bac01f873c6
alberto234 <eyong.ns@gmail.com>
2015-06-10 16:33:32 -0400

    Deleting unneeded file

--- deploy.cmd
+++
@@ -1,3 +0,0 @@
-copy C:\Users\Eyong\development\source_code\GitHub\wurrd\mibew\plugins\scratch-pad\Plugin.php C:\wamp\www\mibew-2b1\plugins\Wurrd\Mibew\Plugin\ScratchPad
-copy C:\Users\Eyong\development\source_code\GitHub\wurrd\mibew\plugins\scratch-pad\routing.yml C:\wamp\www\mibew-2b1\plugins\Wurrd\Mibew\Plugin\ScratchPad
-copy C:\Users\Eyong\development\source_code\GitHub\wurrd\mibew\plugins\scratch-pad\Controller\RouteTestController.php C:\wamp\www\mibew-2b1\plugins\Wurrd\Mibew\Plugin\ScratchPad\Controller

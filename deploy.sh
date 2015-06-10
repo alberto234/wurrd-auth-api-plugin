00b8e9b734c0029d86dc58e45b079ce891727afc
alberto234 <eyong.ns@gmail.com>
2015-06-10 16:33:07 -0400

    Deleting unneeded file

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-cp *.php			../../../../../../MAMP/htdocs/mibew-2b2/plugins/Wurrd/Mibew/Plugin/AuthAPI/
-cp *.yml			../../../../../../MAMP/htdocs/mibew-2b2/plugins/Wurrd/Mibew/Plugin/AuthAPI/
-cp Controller/*		../../../../../../MAMP/htdocs/mibew-2b2/plugins/Wurrd/Mibew/Plugin/AuthAPI/Controller/
-cp Classes/*		../../../../../../MAMP/htdocs/mibew-2b2/plugins/Wurrd/Mibew/Plugin/AuthAPI/Classes/
-cp Model/*			../../../../../../MAMP/htdocs/mibew-2b2/plugins/Wurrd/Mibew/Plugin/AuthAPI/Model/
